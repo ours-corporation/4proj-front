fc72e8ff0538cea8c081ae4bbdf47af08f706b0b
jousset <tom.jousset@etu.unicaen.fr>
2026-01-16 15:28:55 +0100

    initalisation des settings

---
+++ app/settings/page.tsx
@@ -0,0 +1,13 @@
+/*import React, {useEffect} from "react";
+import { useState } from "react";*/
+import Layout from '@/src/components/Layout';
+export default function SettingsPage() {
+
+    return (
+        <Layout currentPage="/settings"><main> <h1> This is the settings page </h1></main>
+        </Layout>
+
+
+    );
+
+}
