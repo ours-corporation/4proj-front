401c3283b9cb2051026b83c4155651514fc5e246
jousset <tom.jousset@etu.unicaen.fr>
2026-01-30 09:23:45 +0100

    oubli de push  updatePassowrd

--- src/api/user.ts
+++ src/api/user.ts
@@ -41,12 +41,38 @@ export async function updateUser(newUsername: string, newEmail: string) {
             },
             body: JSON.stringify({username: newUsername, email: newEmail})
         });
-        console.log(JSON.stringify({user : newUsername, email: newEmail}));
         if (rep.ok) {
             const data: User = await rep.json();
-            console.log(data);
             return data;
         } else {
+            throw new Error(`Erreur lors de la récupération des informations utilisateur : ${rep.status}`);
+        }
+    } catch (error) {
+        throw new Error(`Erreur lors de la récupération des informations utilisateur : ${error}`);
+
+    }
+}
+
+export async function updatePassword(oldPassword: string, newPassword: string) {
+    try {
+        const url = process.env.NEXT_PUBLIC_API_URL
+
+        const rep = await fetch( url + "/api/users/me/update-password", {
+            method: "PUT",
+            credentials: 'include',
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${getJwtToken()}`,
+            },
+            body: JSON.stringify({lastPassword: oldPassword, NewPassword: newPassword})
+        });
+        console.log(JSON.stringify({lastPassword: oldPassword, NewPassword: newPassword}));
+        if (rep.ok) {
+            const data: User = await rep.json();
+            return data;
+        } else {
+            const errorData = await rep.json();
+            console.error("Erreur backend :", errorData);
             throw new Error(`Erreur lors de la récupération des informations utilisateur : ${rep.status}`);
         }
     } catch (error) {
