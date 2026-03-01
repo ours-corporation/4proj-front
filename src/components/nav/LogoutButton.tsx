1e46f24cabd220385fa56610183f2a1883db26f1
jousset <tom.jousset@etu.unicaen.fr>
2026-03-01 14:21:02 +0100

    suppression des cookies d'acces

--- src/components/nav/LogoutButton.tsx
+++ src/components/nav/LogoutButton.tsx
@@ -22,8 +22,9 @@ export default function LogoutButton({isActive = false}:LogoutButtonProps){
         const rep = await logout();
         if (rep.status == 200){
             console.log(rep.status);
+            localStorage.removeItem("accessToken");
+            sessionStorage.removeItem("accessToken");
             router.replace("/login");
-            
         }
         } catch (err) {
         console.error(err);
