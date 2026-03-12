a040f7e38a8dbaf41793435ed65d401882175a9c
TomJous <tomjous@gmail.com>
2026-03-12 13:32:36 +0100

    boutton qui vide la corbeille fonctionnel

--- app/trash/page.tsx
+++ app/trash/page.tsx
@@ -6,11 +6,15 @@ import Loading from '@/src/components/Loading';
 import Layout from '@/src/components/layout/Layout';
 import { getTrashAPI, TrashResponse } from '@/src/api/trash';
 import Folders from '@/src/components/Folders';
+import Modal from "@/src/components/modal/Modal";
+import DeleteTrashModal from "@/src/components/modal/DeleteTrash";
 
 export default function TrashPage() {
     const [trashData, setTrashData] = useState<TrashResponse | null>(null);
     const [loading, setLoading] = useState(true);
     const [viewMode, setViewMode] = useState<'grid' | 'list'>('grid');
+    const [cleanTrashOpen, setCleanTrashOpen] = useState<boolean>(false);
+
 
     useEffect(() => {
         getTrashAPI()
@@ -57,6 +61,21 @@ export default function TrashPage() {
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 6.75h12M8.25 12h12m-12 5.25h12M3.75 6.75h.007v.008H3.75V6.75Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0ZM3.75 12h.007v.008H3.75V12Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm-.375 5.25h.007v.008H3.75v-.008Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
                             </svg>
                         </button>
+
+                        <button name="clear-trash" className="flex flex-row bg-action dark:bg-dark-action hover:bg-action-hover dark:hover:bg-dark-action-hover text-white font-semibold py-2 px-4 rounded-lg shadow-md transition duration-300 mr-4"
+                        onClick={() => setCleanTrashOpen(true)}
+                    >
+                       <div className="flex justify-center items-center mr-3">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"/>
+                                <g transform="scale(1.5) translate(-3,-3)">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12"/>
+                                </g>
+                            </svg>
+                        </div>
+
+                        Vider la corbeille
+                    </button>
                     </div>
                 )}
             </div>
@@ -77,6 +96,12 @@ export default function TrashPage() {
                     contextPermission="READ"
                 />
             )}
+            <DeleteTrashModal
+            isOpen={cleanTrashOpen}
+            closeModal = {() => setCleanTrashOpen(false)}
+            >
+
+            </DeleteTrashModal>
         </Layout>
     );
 }
