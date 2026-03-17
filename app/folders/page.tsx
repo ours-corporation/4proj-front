9ceb8ae5e6e5f1a454f5d65d4440ce78dba6a764
TomJous <tomjous@gmail.com>
2026-03-17 15:00:47 +0100

    idem pour folders

--- app/folders/page.tsx
+++ app/folders/page.tsx
@@ -43,19 +43,18 @@ export default function ShowFolders() {
     const [ isUploading, setIsUploading ] = useState<boolean>(false);
 
 
+    async function fetchFolderData() {
+        setError(null);
+        try {
+            const data = await getFolderById({ folderId });
+            setFolderData(data);
+        } catch {
+            setFolderData(null);
+        }
+    }
+
     useEffect(() => {
-        const fetchData = async () => {
-            setError(null);
-            try {
-                const data = await getFolderById({ folderId });
-                setFolderData(data);
-            } catch {
-                setFolderData(null);
-            }
-        };
-
-        fetchData();
-
+        fetchFolderData()
     }, [folderId]);
 
     async function changeFolderId(newFolderIdNumber: number | null) {
@@ -170,6 +169,17 @@ export default function ShowFolders() {
 
                 <div className="flex flex-row items-center space-x-4">
                     <div className="flex items-center bg-surface dark:bg-dark-surface rounded-lg p-1 space-x-1">
+                       
+                        <button 
+                            onClick={fetchFolderData}
+                            className='p-1.5 rounded-md transition-colors text-txt-secondary hover:bg-gray-100 dark:hover:bg-gray-700 ml-2'
+                            title='rafraichir'
+                        >
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99" />
+                            </svg>
+                        </button>
+                       
                         <button
                             onClick={() => setViewMode('grid')}
                             className={`p-1.5 rounded-md transition-colors ${viewMode === 'grid' ? 'bg-action dark:bg-dark-action text-white' : 'text-txt-secondary dark:text-dark-txt-secondary hover:bg-gray-100 dark:hover:bg-gray-700'}`}
